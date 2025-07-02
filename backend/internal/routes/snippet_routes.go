7b14d05a38db8cd984f2424d9c9732fd71d6acf0
Abhishek Sharma <abhshake17@gmail.com>
2025-07-03 00:42:42 +0530

    snippet routes implementation

--- backend/internal/routes/snippet_routes.go
+++ backend/internal/routes/snippet_routes.go
@@ -1,1 +1,33 @@
 package routes
+
+import (
+	"net/http"
+
+	"github.com/abh1SHAKE/go-git-it/backend/internal/controllers"
+	"github.com/abh1SHAKE/go-git-it/backend/internal/db"
+	"github.com/abh1SHAKE/go-git-it/backend/internal/middleware"
+	"github.com/abh1SHAKE/go-git-it/backend/internal/repository"
+	"github.com/go-chi/chi/v5"
+)
+
+func SnippetRoutes() http.Handler {
+	r := chi.NewRouter()
+
+	conn := db.GetDB()
+	snippetRepo := repository.NewSnippetRepository(conn)
+	snippetController := controllers.NewSnippetController(snippetRepo)
+
+	r.Get("/", snippetController.GetAllPublicSnippets)
+	r.Get("/{id}", snippetController.GetSnippetByID)
+
+	r.Group(func(protected chi.Router) {
+		protected.Use(middleware.AuthMiddleware)
+
+		protected.Post("/", snippetController.CreateSnippet)
+		protected.Get("/me", snippetController.GetMySnippets)
+		protected.Put("/{id}", snippetController.UpdateSnippet)
+		protected.Delete("/{id}", snippetController.DeleteSnippet)
+	})
+
+	return r
+}
