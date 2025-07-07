c45e43dc88527f2781b936b34691c3a849a7b56f
Abhishek Sharma <abhshake17@gmail.com>
2025-07-07 14:25:38 +0530

    wip: landing page

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { LandingComponent } from "./landing/landing.component";
+import { CustomCursorComponent } from "./shared/custom-cursor/custom-cursor.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [LandingComponent, CustomCursorComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'frontend';
 }
