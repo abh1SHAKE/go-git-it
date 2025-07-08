fb1657040a1950e3bbb438a21ae7b1e6d4f2d74d
Abhishek Sharma <abhshake17@gmail.com>
2025-07-08 21:32:51 +0530

    design changes and fixes

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { LandingComponent } from "./landing/landing.component";
 import { CustomCursorComponent } from "./shared/custom-cursor/custom-cursor.component";
+import { DetailsSectionComponent } from "./sections/details-section/details-section.component";
 
 @Component({
   selector: 'app-root',
-  imports: [LandingComponent, CustomCursorComponent],
+  imports: [LandingComponent, CustomCursorComponent, DetailsSectionComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
