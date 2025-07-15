8d85e49fb7955f14269fd2aa5e64d83eb34b0268
Abhishek Sharma <abhshake17@gmail.com>
2025-07-15 18:20:17 +0530

    implement auth panel 2.0

--- frontend/src/app/shared/auth-panel/auth-panel.component.ts
+++ frontend/src/app/shared/auth-panel/auth-panel.component.ts
@@ -1,23 +1,30 @@
 import { NgClass } from '@angular/common';
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, output } from '@angular/core';
+import { AuthRegisterComponent } from "./auth-register/auth-register.component";
+import { AuthLoginComponent } from './auth-login/auth-login.component';
 
 @Component({
   selector: 'app-auth-panel',
   standalone: true,
-  imports: [NgClass],
+  imports: [NgClass, AuthRegisterComponent, AuthLoginComponent],
   templateUrl: './auth-panel.component.html',
   styleUrls: ['./auth-panel.component.scss'],
 })
 export class AuthPanelComponent {
-  @Output() close = new EventEmitter<void>();
+  close = output<void>();
   isClosing = false;
+
+  isSignUpPage = true;
+
+  toggleAuth() {
+    this.isSignUpPage = !this.isSignUpPage;
+  }
 
   onClose() {
     this.isClosing = true;
 
-    // Wait for animation to finish before emitting close
     setTimeout(() => {
       this.close.emit();
-    }, 600); // should match exit animation duration
+    }, 600);
   }
 }
