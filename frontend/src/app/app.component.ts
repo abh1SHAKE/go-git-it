00de40b0605e97f31a97edfd7d5edd002ceb00bd
Abhishek Sharma <abhshake17@gmail.com>
2025-07-19 01:02:43 +0530

    move custom cursor logic to root

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CustomCursorComponent } from "./shared/custom-cursor/custom-cursor.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CustomCursorComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
