428f16c439731f2e228f43dc136a5db7b794667e
Abhishek Sharma <abhshake17@gmail.com>
2025-07-20 14:30:47 +0530

    wip: dashboard

--- frontend/src/app/dashboard/dashboard.component.ts
+++ frontend/src/app/dashboard/dashboard.component.ts
@@ -1,11 +1,42 @@
 import { Component } from '@angular/core';
+import { SnippetCardComponent } from '../shared/snippet-card/snippet-card.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  imports: [FormsModule, SnippetCardComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
 export class DashboardComponent {
+  searchTerm: string = '';
 
+  snippets = [
+    {
+      title: "title1",
+    },
+    {
+      title: "title1",
+    },
+    {
+      title: "title1",
+    },
+    {
+      title: "title1",
+    },
+    {
+      title: "title1",
+    },
+    {
+      title: "title1",
+    },
+    {
+      title: "title1",
+    },
+  ];
+
+  onSearch(term: string) {
+    const lowerTerm = term.toLowerCase();
+    console.log(lowerTerm);
+  }
 }
---
+++ frontend/src/app/shared/snippet-card/snippet-card.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-snippet-card',
+  imports: [],
+  templateUrl: './snippet-card.component.html',
+  styleUrl: './snippet-card.component.scss'
+})
+export class SnippetCardComponent {
+
+}
