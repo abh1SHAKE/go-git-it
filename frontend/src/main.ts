fa8bdc40505d1b9e853f6aaa196d14d6be4129f6
Abhishek Sharma <abhshake17@gmail.com>
2025-07-24 12:22:30 +0530

    monaco editor: fix console errors

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,6 +1,26 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import * as monaco from 'monaco-editor';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+(self as any).MonacoEnvironment = {
+  getWorkerUrl: function (moduleId: string, label: string) {
+    return `data:text/javascript;charset=utf-8,${encodeURIComponent(`
+      self.onmessage = function() {};
+    `)}`;
+  },
+};
+
+monaco.languages.typescript.typescriptDefaults.setDiagnosticsOptions({
+  noSemanticValidation: true,
+  noSyntaxValidation: true,
+});
+
+monaco.languages.typescript.javascriptDefaults.setDiagnosticsOptions({
+  noSemanticValidation: true,
+  noSyntaxValidation: true,
+});
+
+bootstrapApplication(AppComponent, appConfig).catch((err) =>
+  console.error(err)
+);
